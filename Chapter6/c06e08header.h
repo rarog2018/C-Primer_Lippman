21e055130357adaaa282d22a039202bf884e9f5e
rarog2018 <rarog91@protonmail.com>
2019-10-14 20:02:17 +0200

    header file

---
+++ Chapter6/c06e08header.h
@@ -0,0 +1,10 @@
+#ifndef CHAPTER_6_H
+#define CHAPTER_6_H
+
+void add_num(int a, int b);
+void ref_add_num(int &x, int &y);
+unsigned long long factorial(unsigned n);
+int absolute(int n);
+int f();
+
+#endif
