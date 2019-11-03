9f219b9950c42ec0ae93976e66609d020fe700d7
rarog2018 <rarog91@protonmail.com>
2019-11-03 21:54:55 +0100

    practical exercise

---
+++ Chapter6/c06e33.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+void print_vec(vector<int> &vec, size_t n);
+int main(void)
+{
+    vector<int> ivec{1, 3, 5, 7, 9, 11};
+    print_vec(ivec, 0);
+    return 0;
+}
+
+void print_vec(vector<int> &vec, size_t n)
+{
+    // base case
+    if (n == vec.size())
+    {
+        return;
+    }
+    // recursive case
+    cout << vec[n] << " ";
+    print_vec(vec, n + 1);
+}
