467cf365a81ee585163a5c8ea688fd1aac42eb62
rarog2018 <rarog91@protonmail.com>
2019-11-09 15:14:52 +0100

    theoretical exercise

---
+++ Chapter10/c10e08.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main(void)
+{
+    // Because it is the iterator which has the effect of adding elements to
+    // a container, the algorithm itself does not.
+    // Algorithms operate on iterators not containers. Algorithm cannot directly
+    // add or remove elements.
+    return 0;
+}
