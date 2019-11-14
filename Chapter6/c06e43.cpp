d558d7a3b3a635a2c79e489d84603bbe6eee40e8
rarog2018 <rarog91@protonmail.com>
2019-11-14 23:51:19 +0100

    theoretical exercise

--- Chapter6/c06e43.cpp
+++ Chapter6/c06e43.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
 using namespace std;
-// 
+// I would put both of them in a header file. First one is a definition
+// of an inline function, which can be defined multiple times in a program
+// but all of the definitions must match exactly.
 inline bool eq(const BigInt &, const BigInt &) { ... }
 void putValues(int *arr, int size);
 int main(void)
