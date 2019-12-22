d2875e61ed4e5a0870c923bd90a28636b5d6780a
rarog2018 <rarog91@protonmail.com>
2019-12-22 15:58:24 +0100

    check

---
+++ Chapter6/c06e51.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+void f();
+void f(int);
+void f(int, int);
+void f(double, double = 3.14);
+int main(void)
+{
+    ///////////////////////         a           ///////////////////////
+    // The call is ambigous => f(double, double = 3.14) is exact match for first
+    // argument and second argument can be converted to doulbe, but f(int, int)
+    // is exact match for second argument and first argument can be converted
+    // to int
+    // error: call of overloaded 'f(double, int)' is ambiguous
+    // f(2.56, 42);
+
+    ///////////////////////         b           ///////////////////////
+    // There are two viable functions: f(int) which is an exact match, and
+    // f(double, double = 3.14) in which second argument is a default argument
+    // so it does not ned to appear in the call and first argument can be
+    // converted to double
+    // The best match is f(int)
+    f(42); // the message is indeed "f(int)"
+
+    ///////////////////////         c           ///////////////////////
+    // There are two viable functions: f(int, int) which is an exact match,
+    // and f(double, double = 3.14) in which both arguments can be converted
+    // from int to double
+    // The best match is f(int, int)
+    f(42, 0); // the message is indeed "f(int, int)"
+
+    ///////////////////////         d           ///////////////////////
+    // There are two viable functions: f(double, doybke) which is an exact match,
+    // and f(int, int) in which both arguments can be converted from double to int
+    // The best match is f(double, double = 3.14)
+    f(2.56, 3.14); // the message is indeed "f(double, double = 3.14)""
+    return 0;
+}
+
+void f()
+{
+    cout << "f()" << endl;
+}
+void f(int)
+{
+    cout << "f(int)" << endl;
+}
+void f(int, int)
+{
+    cout << "f(int, int)" << endl;
+}
+void f(double, double)
+{
+    cout << "f(double, double = 3.14)" << endl;
+}
