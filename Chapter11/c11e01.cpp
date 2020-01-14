09459dc932e2d6444b51f0f5a56ce81b61b95209
rarog2018 <rarog91@protonmail.com>
2020-01-14 17:29:46 +0100

    theoretical exercise

---
+++ Chapter11/c11e01.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+    /*
+    1. Vector is defined by one type, while map is defined by two types - the
+    type of the key and the type of the associated value.
+    vector<int> ivec;
+    map<string, int> simap;
+
+    2. In map a given key may appear only once, while in vector its values can
+    occur multiple times.
+
+    3. In map each key is associated with a particular value, and this value
+    is found by the key, while the values of vector are found by their position.
+    */
+    return 0;
+}
---
+++ Chapter11/c11e02.cpp
@@ -0,0 +1,18 @@
+int main(void)
+{
+    /*
+    set - when we want to know if the particular value is present
+
+    map - when we want to create a dictionary
+
+    vector - when performance matter and we do not know which container to use :)
+
+    deque - when we need to quickly and efficiently insert and delete the 
+    elements at the beginning and at the end of a container
+
+    list - when we do not need random access and we do not know how many 
+    elements there will be and (or) we need to insert and delete the elements
+    at a random position in the container
+    */
+    return 0;
+}
