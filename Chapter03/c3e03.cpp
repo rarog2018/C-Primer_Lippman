0af03fbd121aff3951199dcc8cbb547a8358d087
rarog2018 <rarog91@protonmail.com>
2020-04-02 09:28:34 +0200

    theoretical exercise

---
+++ Chapter03/c3e03.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+    ////////////////////////////////////////////////////////////////////
+    // string input operator ">>" ignores all the whitespaces
+    // getline() ignores only the newline character
+    return 0;
+}
