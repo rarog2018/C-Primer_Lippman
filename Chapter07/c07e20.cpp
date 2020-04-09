d10605404935b55fa9890791bb4ed0b3ef3faf6c
rarog2018 <rarog91@protonmail.com>
2020-04-09 11:40:22 +0200

    theoretical exercise

---
+++ Chapter07/c07e20.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+// Cons: friend declarations add couple extra lines of code inside class body,
+// when implementation changes it might be necessary to change friend function's
+// code
+// Pros: access to the private members of the class
+int main(void)
+{
+    return 0;
+}
---
+++ Chapter07/c07e25.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+// Yes, 'Screen' can safely rely on the default versions of copy and assignment
+// because all of its members are of type 'string' which defines these operations
+int main(void)
+{
+    return 0;
+}
