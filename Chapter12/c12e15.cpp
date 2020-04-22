59341a3d9c0a3c33f5ecb5d726784886af50e20a
rarog2018 <rarog91@protonmail.com>
2020-04-22 13:51:49 +0200

    practical exercise

---
+++ Chapter12/c12e15.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <memory>
+#include <string>
+using namespace std;
+
+// represents what we are connecting to
+struct destination
+{
+    static unsigned counter;
+    // members for realism purposes
+    string destName;
+    unsigned destId;
+    // default constructor
+    destination(string dN = "", unsigned dI = counter) : destName(dN), destId(dI) { ++counter; }
+};
+
+// information needed to use the connection
+struct connection
+{
+    // pointer to destination
+    destination *conToDest;
+    // non-default constructor
+    connection(destination *dest) : conToDest(dest) {}
+};
+
+connection connect(destination *dest); // open the connection
+void disconnect(connection p);         // close the given connection
+
+void f(destination &dest);
+void end_connection(connection *p) { disconnect(*p); }
+
+unsigned destination::counter = 0;
+int main(void)
+{
+    destination obj("Internet"), obj2("Cloud");
+    f(obj);
+    f(obj2);
+    return 0;
+}
+
+void f(destination &dest)
+{
+    // get a connection; must remember to close it when done
+    connection c = connect(&dest);
+    // use lambda instead of a function
+    shared_ptr<connection> p(&c, [](connection *c) { disconnect(*c); });
+    // use the connection
+    cout << "Using connection: " << p->conToDest->destName << ", id: "
+         << p->conToDest->destId << endl;
+}
+
+connection connect(destination *dest)
+{
+    // return a connection object
+    return connection(dest);
+}
+
+void disconnect(connection p)
+{
+    cout << "Disconnecting from: " << p.conToDest->destName << ", id: "
+         << p.conToDest->destId << endl;
+}
