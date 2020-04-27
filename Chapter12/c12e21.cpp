d4ed2af162bec655dcde9fc236fb9a4c2db92689
rarog2018 <rarog91@protonmail.com>
2020-04-27 15:33:17 +0200

    theoretical exercise

---
+++ Chapter12/c12e21.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/*
+std::string& deref() const
+{ return (*check(curr, "dereference past end"))[curr]; }
+*/
+// IMO the two statement version is way more clear, at first i had no idea what
+// was going on, only after a moment i realized that this is equvalent to
+// (*p)[curr].
+// Although seeing it n-th time makes me think that it is worth getting used to
+// writting statements like this, because it saves space
+
+int main(void)
+{
+    return 0;
+}
