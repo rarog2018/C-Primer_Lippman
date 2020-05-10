8dff3bcbec4462974a0ffe0dd676eab6c75ca84a
rarog2018 <rarog91@protonmail.com>
2020-05-10 10:16:57 +0200

    theoretical exercise

---
+++ Chapter12/c12e31.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// Thanks to using set we do not need to worry about duplicates, but counting
+// occurences becomes a problem
+// With vector it would be easier to count occurences, but we would have to
+// do extra work to avoid printing the same line multiple times
+
+int main(void)
+{
+    return 0;
+}
