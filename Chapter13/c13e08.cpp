386d59a44e2d3156163be83d89d094a757bdc965
rarog2018 <rarog91@protonmail.com>
2020-05-26 17:15:51 +0200

    practical exercise

---
+++ Chapter13/c13e08.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class HasPtr
+{
+public:
+    HasPtr(const std::string &s = std::string()) : ps(new std::string(s)), i(0) {}
+    // copy constructor declaration
+    HasPtr(const HasPtr &o);
+    // assignment operator declaration
+    HasPtr &operator=(const HasPtr &);
+
+    void print() const;
+
+private:
+    std::string *ps;
+    int i;
+};
+
+// assignment operator definition
+HasPtr &HasPtr::operator=(const HasPtr &rhs)
+{
+    cout << "Using assignment operator" << endl;
+    delete ps; // delete old string
+    ps = new string(*rhs.ps);
+    i = rhs.i;
+    return *this;
+}
+
+// copy constructor definition
+HasPtr::HasPtr(const HasPtr &o) : ps(new string(*o.ps)), i(o.i) {}
+
+// print the addresses
+void HasPtr::print() const
+{
+    cout << &this->ps << " " << this->ps << " " << *this->ps << endl;
+}
+int main(void)
+{
+    HasPtr abc("Witcher");
+    HasPtr cde("LOTR");
+    cde.print();
+    cde = abc;
+
+    abc.print();
+    cde.print();
+    return 0;
+}
