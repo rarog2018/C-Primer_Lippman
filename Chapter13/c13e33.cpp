a6d20002cf7720c5d78957d7dc8731d9cc7c2c56
rarog2018 <rarog91@protonmail.com>
2020-07-05 15:45:41 +0200

    theoretical exercise

---
+++ Chapter13/c13e33.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+// The parameter is a reference to the Folder object, because we want to work
+// on the original Folder object, not the copy of it. It is not const, because
+// we want to change one of the object's members.
+
+int main(void)
+{
+    return 0;
+}
