b21b14a8145c53413adfa31f926c52ba0d4ee7f5
rarog2018 <rarog91@protonmail.com>
2020-07-16 15:48:01 +0200

    theoretical exercise

---
+++ Chapter13/c13e38.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// swap() does a lot of operations on both left and right hand objects, while
+// our assignment operator focuses on preparing left hand object and copying
+// to it from the right hand object
+// in short: it does less job, because it doesn't do unnecessary copying to
+// the right hand object
+
+int main(void)
+{
+    return 0;
+}
