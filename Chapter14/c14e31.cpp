fe1540f4bf55145e2d92549f46e8ef481a122064
rarog2018 <rarog91@protonmail.com>
2020-08-20 13:22:01 +0200

    theoretical exercise

---
+++ Chapter14/c14e31.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// All members of StrBlobPtr can be copied, assigned and destroyed correctly
+// with the synthesized members, so there is no need to define them
+
+// members of StrBlobPtr:
+// shared_ptr<vector<string>> check(size_t i, const string &msg) const;
+// weak_ptr<vector<string>> wptr;
+// size_t curr; // current position within the array
+
+int main(void)
+{
+    return 0;
+}
