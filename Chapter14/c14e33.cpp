4240b37ae03b9697ef30d2516e96b6ab38ddb52a
rarog2018 <rarog91@protonmail.com>
2020-08-21 15:08:09 +0200

    theoretical exercise

---
+++ Chapter14/c14e33.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// It can take as many operands as is the maximum number of arguments that the
+// function call can take
+
+int main(void)
+{
+    return 0;
+}
