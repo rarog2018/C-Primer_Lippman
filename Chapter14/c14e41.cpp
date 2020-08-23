deff6d99c6dd01da3c29b434f4138ec8331f0a1a
rarog2018 <rarog91@protonmail.com>
2020-08-23 12:06:54 +0200

    theoretical exercise

---
+++ Chapter14/c14e41.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// New standard added lambdas, because they are very convenient to use in many
+// situations especially with the generic algorithms.
+// I would use lambda over function-object most of the times
+// The only situations in which i would define a function-object class would be
+// if there were some extra processing necessary, but i can't think of such
+// situation in the context of generic algorithms.
+
+int main(void)
+{
+    return 0;
+}
