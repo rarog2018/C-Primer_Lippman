bb445feb0074639a725dac49903477b11cc1b780
rarog2018 <rarog91@protonmail.com>
2020-09-14 13:45:28 +0200

    theoretical exercise

---
+++ Chapter15/c15e24.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// Base classes need virtual destructors. It allows objects in the inheritance
+// hierarchy to be dynamically allocated and destroyed.
+
+int main(void)
+{
+    return 0;
+}
