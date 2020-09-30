fa284a49cfbb9187bca2d8fbc56d657d0857c979
rarog2018 <rarog91@protonmail.com>
2020-09-30 13:59:37 +0200

    theoretical exercise

---
+++ Chapter15/c15e33.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// Query_base class is an abstract class so we cannot directly create an object
+// of its type
+// What happens to the objects that have the type of the class derived from the
+// Query_base depends on the definitions of such classes
+
+int main(void)
+{
+    return 0;
+}
