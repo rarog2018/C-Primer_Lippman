48476c2e7aca535423bba4c5ad6b32902a6286ab
rarog2018 <rarog91@protonmail.com>
2020-11-05 14:45:52 +0100

    theoretical exercise

---
+++ Chapter16/c16e08.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+// The rationale for this habit is that using != operator minimizes the number
+// of requirements, because it is more likely that there will be != operator
+// definition
+
+int main(void)
+{
+    return 0;
+}
